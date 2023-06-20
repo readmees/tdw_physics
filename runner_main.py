63a594003bd45361f7b20d88e3a792886f72b73c
Mees <contact@meesmeester.nl>
2023-06-20 23:47:17 +0200

    Multiple trials without running scene for simple collision, works for segmentation data, background is saved. Frames get saves and deleted for every trial, but saving them as mp4 in the meantime does not work yet

--- runner_main.py
+++ runner_main.py
@@ -17,7 +17,7 @@ import shutil
 # Added for collisions
 import random
 
-
+import os
 
 class Collision(Controller):
     
@@ -37,7 +37,7 @@ class Collision(Controller):
         commands.extend(self.get_add_physics_object(model_name="iron_box",
                                                     object_id=o_id1,
                                                     position={"x": 0, "y": 0, "z": 1}))
-        commands.extend(self.get_add_physics_object(model_name="rh10",
+        commands.extend(self.get_add_physics_object(model_name="iron_box",
                                                 object_id=o_id2,
                                                 position={"x": 1, "y": 0, "z": 1}))
         commands.extend([{"$type": "object_look_at",
@@ -48,37 +48,58 @@ class Collision(Controller):
                           "id": o_id2}])
         return commands
     
-    def trial(self, path):
+    def trial(self, png=False):
         '''
-        param path: "Images will be save to here"'''
+        param png: If True, images will be lossless png files.
+        '''
         # Clear the list of add-ons.
         self.add_ons.clear()
 
         # Add camera
+        main_cam = "collision"
         camera = ThirdPersonCamera(position={"x": 2, "y": 1.6, "z": -1},
                            look_at={"x": 0, "y": 0, "z": 0},
-                           avatar_id="collision")
+                           avatar_id=main_cam)
         self.add_ons.append(camera)
- 
-        # Remove previous images and videos
+        
+        # Define path for output data frames
+        path_main = 'data'
+        paths = [f'{path_main}/{name}/{main_cam}' for name in ['frames_temp', 'backgrounds', 'videos']]
+        path_frames, path_backgr, path_videos = paths
+
+        # Make sure paths exist
+        for path in paths:
+            os.makedirs(path, exist_ok=True)
+
+        # Generate random id for this set of trials, and output for user
+        trial_id = random.randint(10**16, 10**17-1) 
+        print(f'The random id of this set of trials will be {trial_id}')
+
+        # Remove previous frames (if possible)
         try:
-            shutil.rmtree(f'{path}collision/')
+            shutil.rmtree(path_frames)
         except FileNotFoundError:
              pass
         
-        # Save output images/frames for video
-        self.add_ons.append(ImageCapture(path=path, avatar_ids=["collision"]))
+        # Save 'normal' output images/frames_temp for video
+        # ["_img", "_id", "_category", "_mask"]
+        self.add_ons.append(ImageCapture(path=path_main+'/frames_temp/', avatar_ids=[main_cam], png=True, pass_masks=["_img"]))
         
         # Create room and set target framerate
         commands = [TDWUtils.create_empty_room(12, 12),
                     {"$type": "set_target_framerate",
-                "framerate": 30}]
+                    "framerate": 30}]
         self.communicate(commands)
+
+
+        # Save scene/background separately
+        shutil.move(f'{path_frames}/img_0000.png', f'{path_backgr}/background_{main_cam}{trial_id}.png') 
         
         for i in range(5):
             o_ids = [self.get_unique_id() for _ in range(2)]
             commands = [{"$type": "send_rigidbodies",
                                       "frequency": "always"}]
+            
             commands = self.collision_force(commands, o_ids[0], o_ids[1])
 
             self.communicate(commands)
@@ -92,20 +113,32 @@ class Collision(Controller):
                                 {"$type": "send_rigidbodies",
                                 "frequency": "never"}])
                 print(f'Object {o_id} deleted')
+            
+            # Call ffmpeg to make video from frames in frames_temp
+            '''NOTE: the %d wildcard in ffmpeg is not safe for extremely large numbers (with 32-bit max 2,147,483,647)
+            Furthermore, large numbers like this might lead to potential performance issues or file system limitations.
+            The frame number will increase across trials, so 10000 trials of 1000 frames is, will lead to 
+            {trial_id}_trial_10000000.mp4. Numbers under 10**9 should be safe,
+            this is all according to ChatGPT 20 Jun 2023'''
+            
+            call(["ffmpeg",
+                "-i", f'{path_frames}/'+"*.png",
+                "-vcodec", "libx264",
+                "-pix_fmt", "yuv420p",
+                f"{path_videos}/{trial_id}_trial_{i}.mp4"])
+            import time
+            time.sleep(1)
+            # Remove frames for next trial
+            shutil.rmtree(path_frames)
+
         self.communicate({"$type": "terminate"})
+
+        # Let the user know where the trial videos are stored
+        print(f'The random id of this set of trials was {trial_id}')
+        print(f'You can find trial n at f"{path_videos}/{trial_id}_trial_n.mp4"')
+
 
 if __name__ == "__main__":
     c = Collision()
-    path = 'data/'
-    success = c.trial(path)
+    success = c.trial()
     print('succes:', success)
-
-    # Change directory.
-    # chdir(str(path.joinpath("a").resolve()))
-
-    # Call ffmpeg to make video
-    call(["ffmpeg",
-        "-i", f"{path}collision/"+"img_%04d.jpg",
-        "-vcodec", "libx264",
-        "-pix_fmt", "yuv420p",
-        f"{path}collision/"+"image_only_video.mp4"])
