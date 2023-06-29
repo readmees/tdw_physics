6f15e7fc7b1e223b8d9ace0938966ee7433bf07f
Mees <contact@meesmeester.nl>
2023-06-29 02:27:27 +0200

    Add objects to use per trial

---
+++ controllers/objects.py
@@ -0,0 +1,179 @@
+CONTAINERS = [
+    '41.b03_696615_object001',
+    '46.b03_basket',
+    '88.b04_basket',
+    '94.b04_bowl_smooth',
+    '317.elephant_bowl',
+    '299.cup',  # Tiny
+    '295.coffeemug',
+    '293.coffeecup004',
+    '294.coffeecup004_fix',
+    '215.box_18inx18inx12in_cardboard',
+    '216.box_24inx18inx12in_cherry',
+    '217.box_tapered_beech',
+    '218.box_tapered_white_mesh',
+    '188.basket_18inx18inx12iin',
+    '189.basket_18inx18inx12iin_bamboo',
+    '190.basket_18inx18inx12iin_plastic_lattice',
+    '191.basket_18inx18inx12iin_wicker',
+    '192.basket_18inx18inx12iin_wood_mesh'
+]
+
+CONTAINED = [
+    '0.034_vray',
+    '1.102_pepsi_can_12_fl_oz_vray',
+    '2.104_sprite_can_12_fl_oz_vray',
+    '3.12_06_001',
+    '15.868580_pliers_max2016',
+    '16.9v_battery',
+    '17.aaa_battery',
+    '18.afl_lamp',
+    '19.alarm_clock',
+    '23.apple',
+    '24.apple_ipod_touch_grey_vray',
+    '36.atmosphere_nodo',
+    '45.b03_banana_01_high',
+    '47.b03_beats_solo_hd_headphone_03_2010',
+    '49.b03_burger',
+    '50.b03_cocacola_can_cage',
+    '56.b03_dollarstack',
+    '57.b03_hair_comb_2010',
+    '58.b03_iron_candle_vray',
+    '61.b03_loafbread',
+    '65.b03_padlock',
+    '66.b03_pen',
+    '67.b03_pen_01_001',
+    '76.b03_spoon_001',
+    '77.b03_toothbrush',
+    '82.b04_3d_jar_180_gr_01',
+    '90.b04_bottle-2014-2018',
+    '91.b04_bottle_20ml',
+    '284.chocolate_bar001',
+    '282.champagne_cork',
+    '259.calculator'
+]
+
+OCCLUDERS = [
+    '4.24_in_wall_cabinet_white_wood',
+    '5.24_in_wall_cabinet_wood_beach_honey',
+    '6.36_in_wall_cabinet_white_wood',
+    '7.36_in_wall_cabinet_wood_beach_honey',
+    '27.appliance-ge-profile-microwave',
+    '28.appliance-ge-profile-microwave3',
+    '31.aquostv',
+    '34.arflex_strips_sofa',
+    '48.b03_bosch_cbg675bs1b_2013__vray_composite',
+    '53.b03_db_apps_tech_08_04_composite',
+    '55.b03_db_apps_tech_08_08_composite',
+    '59.b03_ka90ivi20r_2013__vray',
+    '310.dishwasher_4_composite',
+    '318.elf_painting',
+    '285.club_house_italia_nautilus_sofa',
+    '286.coffee_001',
+    '287.coffee_0023',
+    '288.coffee_005_13',
+    '289.coffee_cup',
+    '290.coffee_grinder',
+    '266.cgaxis_models_10_11_vray',
+    '236.cabinet_24_door_drawer_wood_beach_honey',
+    '237.cabinet_24_single_door_wood_beech_honey_composite',
+    '238.cabinet_24_single_door_wood_oak_white_composite',
+    '239.cabinet_24_singledoor_wood_beach_honey',
+    '240.cabinet_24_two_door_wood_beech_honey_composite',
+    '241.cabinet_24_two_door_wood_oak_white_composite',
+    '242.cabinet_24_two_drawer_white_wood',
+    '243.cabinet_24_two_drawer_wood_beach_honey',
+    '244.cabinet_24_wall_wood_beech_honey_composite',
+    '245.cabinet_24_wall_wood_oak_white_composite',
+    '246.cabinet_24_white_wood',
+    '247.cabinet_24_wood_beach_honey',
+    '248.cabinet_36_two_door_wood_beech_honey_composite',
+    '249.cabinet_36_two_door_wood_oak_white_composite',
+    '250.cabinet_36_wall_wood_beech_honey_composite',
+    '251.cabinet_36_wall_wood_oak_white_composite',
+    '252.cabinet_36_white_wood',
+    '253.cabinet_36_wood_beach_honey',
+    '254.cabinet_full_height_white_wood',
+    '255.cabinet_full_height_wood_beach_honey',
+    '256.cabinet_full_height_wood_beech_honey_composite',
+    '257.cabinet_full_height_wood_oak_white_composite',
+    '195.bed01',
+    '196.bed01_blue',
+    '197.bed01_red',
+    '181.balance_double_doghouse'
+]
+
+OCCLUDERS_SEE_THROUGH = [
+    '8.4ft_shelf_metal',
+    '8.4ft_shelf_metal',
+    '9.4ft_wood_shelving',
+    '10.5ft_shelf_metal',
+    '11.5ft_wood_shelving',
+    '12.699264_shoppingcart_2013',
+    '13.6ft_shelf_metal',
+    '14.6ft_wood_shelving',
+    '70.b03_radiator_alum_12',
+    '73.b03_shopping_cart',
+    '74.b03_shopping_cart_walmart',
+    '75.b03_shoppingcart_2013'
+]
+
+ROLLING_FLIPPED = [
+    '1.102_pepsi_can_12_fl_oz_vray',
+    '2.104_sprite_can_12_fl_oz_vray',
+    '16.9v_battery',
+    '17.aaa_battery',
+    '50.b03_cocacola_can_cage',
+    '90.b04_bottle-2014-2018',
+    '91.b04_bottle_20ml',
+    '92.b04_bottle_2_max',
+    '93.b04_bottle_max'
+]
+
+BALLS = [
+    '12.699264_shoppingcart_2013',
+    '23.apple',
+    '72.b03_roller_skate',
+    '73.b03_shopping_cart',
+    '74.b03_shopping_cart_walmart',
+    '75.b03_shoppingcart_2013'
+]
+
+FAULTY = [
+    '128.b04_wallmounted_soap_dispenser_composite'
+]
+
+CONTAINERS = {
+    item.split('.')[1]: int(item.split('.')[0])
+    for item in CONTAINERS
+}
+
+CONTAINED = {
+    item.split('.')[1]: int(item.split('.')[0])
+    for item in CONTAINED
+}
+
+OCCLUDERS = {
+    item.split('.')[1]: int(item.split('.')[0])
+    for item in OCCLUDERS
+}
+
+OCCLUDERS_SEE_THROUGH = {
+    item.split('.')[1]: int(item.split('.')[0])
+    for item in OCCLUDERS_SEE_THROUGH
+}
+
+ROLLING_FLIPPED = {
+    item.split('.')[1]: int(item.split('.')[0])
+    for item in ROLLING_FLIPPED
+}
+
+BALLS = {
+    item.split('.')[1]: int(item.split('.')[0])
+    for item in BALLS
+}
+
+FAULTY = {
+    item.split('.')[1]: int(item.split('.')[0])
+    for item in FAULTY
+}
